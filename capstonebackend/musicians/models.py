a0ce4d0977119a98ad504da87610169ab1807a9f
MeKinley <mekinleydsaintvictor@outlook.com>
2021-10-11 11:55:03 -0400

    Created musicians app, model and registered with admin site.

--- capstonebackend/musicians/models.py
+++ capstonebackend/musicians/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Musician(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    aboutMe = models.CharField(max_length=500)
+    video = models.CharField(max_length=500)
+    profilePic = models.ImageField()
+    jam = models.IntegerField()
+    #influences/fk, band/fk
+    #influences - artist, genre, band pic
+    #band - bandname, members, bandSInce
+    def __str__(self):
+        return self.name
